e599b277d97c5cc4cca4c241c9c8d0c20059a476
Tay Hong Bing <ashton@Tays-MacBook-Pro.local>
2024-01-30 17:58:45 +0800

    18. Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import config from '../config.json'
+import config from '../config.json';
 
 import { 
   loadProvider, 
@@ -9,6 +9,8 @@ import {
   loadTokens,
   loadExchange
 } from '../store/interactions';
+
+import Navbar from './Navbar';
 
 function App() {
   const dispatch = useDispatch()
@@ -20,8 +22,16 @@ function App() {
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
     // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch)
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch)
+    })
+    // await loadAccount(provider, dispatch)
 
     // Load token Smart Contract
     const DApp = config[chainId].DApp
@@ -40,7 +50,7 @@ function App() {
   return (
     <div>
 
-      {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
